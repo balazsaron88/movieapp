90930068a449ca951ecd0f36f88fdcc6c040c969
Balazs Aron <balazsaron88@gmail.com>
2019-12-17 08:25:46 +0200

    Added home fragment to list the popular movies

--- app/src/main/java/com/example/movieapp/DashboardActivity.java
+++ app/src/main/java/com/example/movieapp/DashboardActivity.java
@@ -1,70 +1,68 @@
 package com.example.movieapp;
-import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import androidx.fragment.app.FragmentManager;
 
+import com.example.movieapp.favorite.FavoritesFragment;
+import com.example.movieapp.home.HomeFragment;
+import com.example.movieapp.profile.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    String EmailHolder;
-    TextView Email;
-    Button LogOUT ;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-        Email = (TextView)findViewById(R.id.textView1);
-        LogOUT = (Button)findViewById(R.id.button1);
-
-        Intent intent = getIntent();
-
-        // Receiving User Email Send By MainActivity.
-        EmailHolder = intent.getStringExtra(LoginActivity.UserEmail);
-
-        // Setting up received email to TextView.
-        Email.setText(Email.getText().toString()+ EmailHolder);
-
-        // Adding click listener to Log Out button.
-        LogOUT.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                //Finishing current DashBoard activity on button click.
-                finish();
-
-                Toast.makeText(DashboardActivity.this,"Log Out Successfull", Toast.LENGTH_LONG).show();
-
-            }
-        });
+        fragmentManager = getSupportFragmentManager();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+        navigateToHome();
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_profile:
-                        Toast.makeText(DashboardActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                        navigateToProfile();
                         break;
                     case R.id.action_home:
-                        Toast.makeText(DashboardActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        navigateToHome();
                         break;
                     case R.id.action_favorite:
-                        Toast.makeText(DashboardActivity.this, "Favorites   ", Toast.LENGTH_SHORT).show();
-                        break;                }
+                        navigateToFavorites();
+                        break;
+                }
                 return true;
             }
 
         });
 
+    }
+
+    private void navigateToProfile() {
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, ProfileFragment.newInstance())
+                .commit();
+    }
+
+    private void navigateToHome() {
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, HomeFragment.newInstance())
+                .commit();
+    }
+
+    private void navigateToFavorites() {
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, FavoritesFragment.newInstance())
+                .commit();
     }
 }
